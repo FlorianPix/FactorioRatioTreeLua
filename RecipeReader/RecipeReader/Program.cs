a74452f26e5c9fda517f8c7bf85839722f7815e5
Alexander Krause <alexander@krause4it.de>
2020-04-20 22:27:56 +0200

    c# init

---
+++ RecipeReader/RecipeReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RecipeReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
