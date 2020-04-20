aefc3ebccd7417a5b849416f0716595d00fcda29
Alexander Krause <alexander@krause4it.de>
2020-04-20 22:36:30 +0200

    c# init2

--- RecipeReader/RecipeReader/Program.cs
+++ RecipeReader/RecipeReader/Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.IO;
 
 namespace RecipeReader
 {
@@ -7,6 +9,16 @@ namespace RecipeReader
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+
+            using (StreamReader r = new StreamReader("file.json"))
+            {
+                string json = r.ReadToEnd();
+                dynamic array = JsonConvert.DeserializeObject(json);
+                foreach (var item in array)
+                {
+                    Console.WriteLine("{0} {1}", item.temp, item.vcc);
+                }
+            }
         }
     }
 }
